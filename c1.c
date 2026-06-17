d1c15d292938b8221790aa60ff67297bc724c5ed
rimu0410 <153588519+rimu0410@users.noreply.github.com>
2026-06-17 11:38:37 +0600

    Add files via upload

---
+++ c1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int A = 50;
+    int B = 20;
+    int C = 10;
+
+    printf("A = %d\n", A);
+    printf("B = %d\n", B);
+    printf("C = %d\n", C);
+    printf("Result = %d\n", A - B - C);
+
+    return 0;
+}
